783d51d547a87b67a0f5524d9f5cfd47aa407cb2
pgtognoni <pgtognoni@gmail.com>
2023-01-20 21:31:01 -0300

    Score btns

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+let height = 0;
+let width = 0;
+
+const canvas = document.querySelector('canvas')
+canvas.style.backgroundColor="blue";
+
+const ctx = canvas.getContext('2d');
+
+const canvasX = canvas.width;
+const canvasY = canvas.height;
+
 function setCanvasSize(){
-    var height = window.innerHeight;
-    var width = window.innerWidth;
+
+    height = window.innerHeight;
+    width = window.innerWidth;
+
     document.getElementById('canvas').style.height = height + "px";
     document.getElementById('canvas').style.width = width + "px";
 
@@ -8,4 +21,9 @@ function setCanvasSize(){
     document.getElementById('canvas').style.width);
 }
 
-window.addEventListener("resize",setWindowHeight,false);
+window.onload = () => {
+
+    setCanvasSize()
+ 
+    window.addEventListener("resize",setCanvasSize,false);
+}
